d85b550a8d2009df826841292b6772f02605c109
02mikuniko <13210443+mikuniko02@user.noreply.gitee.com>
2025-03-04 18:37:19 +0800

    创建ACGN项目

---
+++ src/main/java/com/example/acgn/AcgnApplication.java
@@ -0,0 +1,13 @@
+package com.example.acgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcgnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/acgn/AcgnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acgn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcgnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
